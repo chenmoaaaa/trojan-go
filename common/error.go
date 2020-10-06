94573dfc8764f7ca4a42a6bd559ada84a697cf36
chenmoaaaa <33976355+chenmoaaaa@users.noreply.github.com>
2020-10-06 16:21:55 +0800

    Update error.go

--- common/error.go
+++ common/error.go
@@ -1,13 +1,14 @@
+//声明所属那个包
 package common
-
+//导入
 import (
 	"fmt"
 )
-
+//创建struct（结构）
 type Error struct {
 	info string
 }
-
+//方法，接受者为指针类型对其修改会影响原始数据
 func (e *Error) Error() string {
 	return e.info
 }
@@ -18,13 +19,13 @@ func (e *Error) Base(err error) *Error {
 	}
 	return e
 }
-
+//工厂变量go语言中New变量一般为工厂变量，用于创建error值
 func NewError(info string) *Error {
 	return &Error{
 		info: info,
 	}
 }
-
+//must函数用于检测到重大错误时终止程序
 func Must(err error) {
 	if err != nil {
 		fmt.Println(err)
